25f850ce53effdf8819cb9a7ab159dd07f752572
mirsahebali <mirsahebali204@gmail.com>
2023-06-25 23:52:56 +0530

    fixing #4

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,10 +1,11 @@
-"use client"
-export default function Home() {
+"use client";
 
-  return (
-    <>
-      <main>
-      </main>
-    </>
-  );
+import { useSession } from "next-auth/react";
+import {redirect} from "next/navigation";
+export default function Home() {
+  const { data: session } = useSession();
+  if (!session) {
+    redirect("/auth/signin");
+  }
+  redirect("/board/page")
 }
