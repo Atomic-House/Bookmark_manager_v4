b1fca329da5ecbbf8648a79f6f8819300c3bbebb
mirsahebali <mirsahebali204@gmail.com>
2023-06-25 22:58:08 +0530

    added dynamic link

--- src/functions/queries.ts
+++ src/functions/queries.ts
@@ -14,7 +14,7 @@ export function useFetchWorkspace() {
   } = useQuery({
     queryKey: ["workspaces"],
     queryFn: async () => {
-      const data = await fetch("http://localhost:3000/api/data/workspace/read", {
+      const data = await fetch("/api/data/workspace/read", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -36,6 +36,39 @@ export function useFetchWorkspace() {
     refetch,
   };
 }
-export function useFetchData(){
-
+export async function useFetchData(category: string) {
+  const {
+    data,
+    isError,
+    isStale,
+    isPaused,
+    isSuccess,
+    isFetching,
+    isLoading,
+    isLoadingError,
+    refetch,
+  } = useQuery({
+    queryKey: [category],
+    queryFn: async (parentId) => {
+      const data = await fetch(`/api/data/${category}/${parentId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        cache: "no-store",
+      });
+      return await data.json();
+    },
+  });
+  return {
+    data,
+    isError,
+    isStale,
+    isPaused,
+    isSuccess,
+    isFetching,
+    isLoading,
+    isLoadingError,
+    refetch,
+  };
 }
