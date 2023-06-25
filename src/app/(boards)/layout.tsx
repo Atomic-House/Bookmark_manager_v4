5e9bdb96b12b16c6e5628881e03ca39b1da4ca2a
mirsahebali <mirsahebali204@gmail.com>
2023-06-25 23:16:44 +0530

    fixed favicon

--- src/app/(boards)/layout.tsx
+++ src/app/(boards)/layout.tsx
@@ -15,6 +15,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body>
         <Providers>
           <Sidebar>
--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,5 +1,5 @@
 import Providers from "../providers";
-import "./globals.css"
+import "./globals.css";
 export const metadata = {
   title: "Login or Signup",
   description: "Created by Mir Saheb Ali",
@@ -7,6 +7,9 @@ export const metadata = {
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
