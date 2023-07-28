d1c6183b48ac279aced3617541da9004dcc6a21b
mirsahebali <mirsahebali204@gmail.com>
2023-07-29 04:55:04 +0530

    auto closing add options

--- src/components/Create/create.tsx
+++ src/components/Create/create.tsx
@@ -11,11 +11,13 @@ import {
   PopoverTrigger,
   Portal,
   Spinner,
+  useDisclosure,
 } from "@chakra-ui/react";
 import {
   ChangeEventHandler,
   FormEventHandler,
   HTMLAttributes,
+  useEffect,
   useRef,
   useState,
 } from "react";
@@ -44,70 +46,79 @@ export default function AddClass({
   isSuccess?: boolean;
 }) {
   const initRef = useRef();
-
   return (
     <div className={positionStyles}>
       <Popover
         closeOnBlur={false}
         placement="bottom"
         initialFocusRef={initRef.current}
-      // isOpen={}
+        // isOpen={}
       >
-        {({ isOpen, onClose }) => (
-          <>
-            <PopoverTrigger>
-              <button
-                className={` ${buttonStyles} w-fit duration-300 transition-all`}
-              >
-                {placeholder}
-              </button>
-            </PopoverTrigger>
-            <Portal>
-              <PopoverContent>
-                <PopoverHeader>
-                  {add_edit}
-                  {category.slice(0, category.length - 1)}
-                </PopoverHeader>
-                <PopoverCloseButton />
-                <PopoverBody>
-                  <form onSubmit={onSubmit}>
-                    <Input
-                      type="text"
-                      name="name"
-                      id="name"
-                      placeholder={category + " name"}
-                      onChange={onChange}
-                      my={2}
-                      p={2}
-                      variant={"unstyled"}
-                    />
-                    <Button
-                      mt={4}
-                      mx={2}
-                      px={9}
-                      bg="#422AFB"
-                      textColor={"white"}
-                      ref={initRef.current}
-                      type="submit"
-                    >
-                      {isLoading ? <Spinner /> : ""} Add
-                    </Button>
-                    <Button
-                      type="button"
-                      mt={4}
-                      textColor={"black"}
-                      colorScheme="white"
-                      onClick={onClose}
-                      ref={initRef.current}
-                    >
-                      Close
-                    </Button>
-                  </form>
-                </PopoverBody>
-              </PopoverContent>
-            </Portal>
-          </>
-        )}
+        {({ isOpen, onClose }) => {
+          return (
+            <>
+              <PopoverTrigger>
+                <button
+                  className={` ${buttonStyles} w-fit duration-300 transition-all`}
+                >
+                  {placeholder}
+                </button>
+              </PopoverTrigger>
+              <Portal>
+                <PopoverContent>
+                  <PopoverHeader>
+                    {add_edit}
+                    {category.slice(0, category.length - 1)}
+                  </PopoverHeader>
+                  <PopoverCloseButton />
+                  <PopoverBody>
+                    <form onSubmit={onSubmit}>
+                      <Input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder={category + " name"}
+                        onChange={onChange}
+                        my={2}
+                        p={2}
+                        variant={"unstyled"}
+                      />
+                      <Button
+                        mt={4}
+                        mx={2}
+                        px={9}
+                        bg="#422AFB"
+                        onClick={onClose}
+                        textColor={"white"}
+                        ref={initRef.current}
+                        type="submit"
+                      >
+                        {isLoading ? (
+                          <Spinner />
+                        ) : (
+                          () => {
+                            return <> </>;
+                          }
+                        )}{" "}
+                        Add
+                      </Button>
+                      <Button
+                        type="button"
+                        mt={4}
+                        textColor={"black"}
+                        colorScheme="white"
+                        onClick={onClose}
+                        ref={initRef.current}
+                      >
+                        Close
+                      </Button>
+                    </form>
+                  </PopoverBody>
+                </PopoverContent>
+              </Portal>
+            </>
+          );
+        }}
       </Popover>
     </div>
   );
