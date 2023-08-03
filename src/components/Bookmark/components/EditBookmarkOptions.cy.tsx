966ccf02bbd295200695b84bf1adfaa1a4b0b123
mirsahebali <mirsahebali204@gmail.com>
2023-08-04 01:59:52 +0530

    debugging #100

--- src/components/Bookmark/components/EditBookmarkOptions.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import EditBookmarkOptions from './EditBookmarkOptions'
-
-describe('<EditBookmarkOptions />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<EditBookmarkOptions />)
-  })
-})
