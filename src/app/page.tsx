a6a25a9f6124f342f1659e2deff5c08f386873b2
mirsahebali <mirsahebali204@gmail.com>
2023-08-04 02:10:28 +0530

    debugging #102

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 export default function Home() {
   const router = useRouter();
   const { data: session } = useSession();
