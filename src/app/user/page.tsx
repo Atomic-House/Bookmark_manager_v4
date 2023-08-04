a77bf2bb817d55cef6e959580aef8ab098072fa6
mirsahebali <mirsahebali204@gmail.com>
2023-08-04 16:00:09 +0530

    router issues debugging #1

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 export default function Home() {
   const router = useRouter();
   const { data: session } = useSession();
-  if (!session) router.push("/user/auth/signin");
-  router.push("/main/home");
+  useEffect(() => {
+    if (session) {
+      router.push("/main/home");
+    }
+    return () => {
+      router.push("/user/auth/signin");
+    };
+  }, [router, session]);
 }
