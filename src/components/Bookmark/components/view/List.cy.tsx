333fd4926fab16ddfa60881dd14d7f91a10890ad
mirsahebali <mirsahebali204@gmail.com>
2023-09-18 17:47:54 +0530

    fixing Cy.tsx files

--- src/components/Bookmark/components/view/List.cy.tsx
+++ src/components/Bookmark/components/view/List.cy.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import ListView from './List'
-import "@/app/globals.css"
-describe('<List />', () => {
-  it('renders', () => {
+import React from "react";
+import ListView from "./List";
+import "@/app/globals.css";
+describe("<List />", () => {
+  it("renders", () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(
-      <span className='flex items-center w-[20-vw] border-white border-2 justify-center'>
-        <ListView url={`/laskjflks/l;aksdjdfa;s`} id='al;sjasldk' title='Google' icon='https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png' />
-      </span>
-    )
-  })
-})
+      <span className="flex items-center w-[20-vw] border-white border-2 justify-center">
+        <ListView
+          url={`/laskjflks/l;aksdjdfa;s`}
+          id="al;sjasldk"
+          title="Google"
+          favicon="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"
+          name={"as;kdfj"}
+        />
+      </span>,
+    );
+  });
+});
