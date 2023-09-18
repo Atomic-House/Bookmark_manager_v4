1ea1f0cee40cfc8443c212614d4058acbb3d5f5c
mirsahebali <mirsahebali204@gmail.com>
2023-09-18 17:41:20 +0530

    cleanup of unused svgs

--- public/svgs/Box.tsx.tsx
+++
@@ -1,7 +0,0 @@
-export default function BOX.TSX() { return (export default function BOX.SVG { return (<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="1.1" y="1.1" width="19.8" height="19.8" rx="1.9" stroke="#65666F" stroke-width="2.2"/>
-</svg>
-) 
- }
-) 
- }
